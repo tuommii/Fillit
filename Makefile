a23f88e2785d094673e419f1eb2cfe25da39ed81
Miikka Tuominen <mtuomine@c2r2p8.hive.fi>
2019-10-29 15:35:12 +0200

    testing compiling

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME		=	fillit
+
+LIBFT_DIR	= 	libft/
+LIBFT_A		= 	libft.a
+LIBFT_H		= 	-I libft/
+FILLIT_H	= 	-I fillit/
+
+OBJ_DIR		= 	obj/
+SRC_DIR		= 	fillit/
+
+COMP		= 	gcc -Wall -Werror -Wextra $(PRINTF_H) $(LIBFT_H) -c -o
+
+CFILE		= 	main.c
+
+SRC =		$(CFILE:%=$(SRC_DIR)%)
+
+OFILE =		$(CFILE:%.c=%.o)
+
+OBJ =		$(addprefix $(OBJ_DIR), $(OFILE))
+
+all:
+	gcc fillit/main.c -I libft -L libft -lft
---
+++ fillit/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 15:04:07 by mtuomine          #+#    #+#             */
+/*   Updated: 2019/10/29 15:05:51 by mtuomine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// TODO: Remove this
+#include <stdio.h>
+#include "libft.h"
+
+int main(int argc, const char **argv)
+{
+	if (!argc || !argv)
+		return (0);
+	ft_putstr("Hello!");
+	return (0);
+}
