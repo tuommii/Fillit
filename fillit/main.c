6d7ed2041cb7fe4230fe748bfd53ea58e18a17f9
Miikka Tuominen <mtuomine@c2r2p8.hive.fi>
2019-10-29 16:18:11 +0200

    gnl works

--- fillit/main.c
+++ fillit/main.c
@@ -6,22 +6,30 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 15:04:07 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/29 16:07:30 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/29 16:17:54 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // TODO: Remove this
 #include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
 #include "libft.h"
 
 int main(int argc, const char **argv)
 {
-	char **line;
+	char *line;
+	int fd;
+	int bytes;
 
 	if (argc != 2)
 		return (0);
 
-	get_next_line();
-	ft_putstr("Hello!");
+	fd = open(argv[1], O_RDONLY);
+	while ((bytes = get_next_line(fd, &line)) > 0)
+	{
+		ft_putstr(line);
+		free(line);
+	}
 	return (0);
 }
