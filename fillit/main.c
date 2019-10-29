ff44da7551228adfda72a7c18c9d46fe3e2835fb
Miikka Tuominen <mtuomine@c2r2p8.hive.fi>
2019-10-29 16:32:13 +0200

    test map added

--- fillit/main.c
+++ fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 15:04:07 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/29 16:17:54 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/29 16:31:46 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 #include <fcntl.h>
 #include "get_next_line.h"
 #include "libft.h"
+#include "fillit.h"
+
+
+void handle_line(const char *line)
+{
+	static int count = 0;
+	count++;
+}
 
 int main(int argc, const char **argv)
 {
@@ -28,8 +36,10 @@ int main(int argc, const char **argv)
 	fd = open(argv[1], O_RDONLY);
 	while ((bytes = get_next_line(fd, &line)) > 0)
 	{
-		ft_putstr(line);
+		handle_line(line);
+		//ft_putstr(line);
 		free(line);
 	}
+	close(fd);
 	return (0);
 }
