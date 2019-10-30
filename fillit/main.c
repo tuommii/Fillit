46fe03a026beaf32d24d3d6e7757ea45707820cc
Miikka Tuominen <mtuomine@c2r2p8.hive.fi>
2019-10-30 10:09:35 +0200

    trying print all list items but iteration is infinite

--- fillit/main.c
+++ fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 15:04:07 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/30 09:32:08 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/10/30 10:08:56 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,31 @@
 
 t_list	*read_tetris_file(int fd)
 {
-	char *buf;
-	size_t bytes;
-	t_list *list;
-
+	char	*buf;
+	size_t	bytes;
+	t_list	*list;
+	t_list	*current;
 	list = ft_lstnew(NULL, 0);
-	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (buf == NULL)
-		return NULL;
+	if (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
+		return (NULL);
 	while ((bytes = read(fd, buf, BUFFER_SIZE + 1)) > 0)
 	{
 		buf[BUFFER_SIZE] = '\0';
-		printf("%s", buf);
-
+		current->content = buf;
+		ft_lstadd(&list, current);
 	}
 	free(buf);
 	close(fd);
 	return list;
+}
+
+void print_node(t_list *node)
+{
+	printf("%s\n", node->content);
+}
+
+void	print_list(t_list *list)
+{
 }
 
 int main(int argc, const char **argv)
@@ -47,6 +55,11 @@ int main(int argc, const char **argv)
 	if (argc != 2)
 		return (0);
 
-	list = read_tetris_file(open(argv[1], O_RDONLY));
+	if (!(list = read_tetris_file(open(argv[1], O_RDONLY))))
+		return (0);
+
+	ft_lstiter(list, print_node);
+	//print_list(list);
+
 	return (0);
 }
