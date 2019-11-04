153f6c8028d1b796757d5bbba4558d99bc2b30e5
Miikka Tuominen <mtuomine@c2r2p8.hive.fi>
2019-11-04 08:57:05 +0200

    lstiter with print functin works

--- fillit/main.c
+++ fillit/main.c
@@ -6,7 +6,7 @@
 /*   By: mtuomine <mtuomine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 15:04:07 by mtuomine          #+#    #+#             */
-/*   Updated: 2019/10/30 10:23:24 by mtuomine         ###   ########.fr       */
+/*   Updated: 2019/11/04 08:56:00 by mtuomine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,23 @@
 #include "fillit.h"
 
 
+t_tetris *create_tetris(char *str)
+{
+	t_tetris *tetris;
+
+	tetris = NULL;
+	tetris = ft_memalloc(sizeof(t_tetris));
+	tetris->buffer = ft_strdup(str);
+	tetris->test = 42;
+	return (tetris);
+}
+
 t_list	*read_tetris_file(int fd)
 {
 	char	*buf;
 	size_t	bytes;
 	t_list	*list;
+	t_tetris *tetris;
 
 	list = NULL;
 	if (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
@@ -32,20 +44,22 @@ t_list	*read_tetris_file(int fd)
 	{
 		buf[BUFFER_SIZE] = '\0';
 		//printf("%s:%zu\n", buf, bytes);
-		ft_lstadd(&list, ft_lstnew((char *)buf, sizeof(buf)));
+		tetris = create_tetris(buf);
+		//printf("tetris:\n%s\n", tetris->buffer);
+		ft_lstadd(&list, ft_lstnew(tetris, sizeof(tetris)));
 	}
 	free(buf);
 	close(fd);
 	return list;
 }
 
-void print_node(t_list *node)
+void	print_node(t_list *node)
 {
-	printf("%s\n", node->content);
-}
+	t_tetris *tetris;
 
-void	print_list(t_list *list)
-{
+	tetris = node->content;
+
+	printf("%s\n", tetris->buffer);
 }
 
 int main(int argc, const char **argv)
@@ -59,7 +73,6 @@ int main(int argc, const char **argv)
 		return (0);
 
 	ft_lstiter(list, &print_node);
-	//print_list(list);
 
 	return (0);
 }
